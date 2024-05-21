905c74df986377ac30b6d1d3ec1f89de6e84566a
Matt Jennings <matt@mattjennings.net>
2024-05-21 15:01:19 -0700

    refactored theme functions to simplify

--- wp-content/themes/MJ-net-2012/inc/wp-core-utils/wp-core-utils.php
+++ wp-content/themes/MJ-net-2012/inc/wp-core-utils/wp-core-utils.php
@@ -7,21 +7,27 @@ class WPCoreUtils {
   add_action('wp_loaded', array($self, 'enqueue_dequeue_styles_scripts'));
 		add_action('body_class', array($self, 'customize_body_class'));
 		add_filter ('wp_nav_menu', array($self, 'customize_nav_menu_output'));
-		add_filter( 'excerpt_length', array($self, 'custom_excerpt_length'), 999 );	
+		add_filter( 'post_class', array($self, 'mark_first_post') );	
+		remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+		add_filter( 'get_the_excerpt', array($self, 'improved_excerpt') );	
 
-		// Add RSS links to <head> section
-  add_theme_support( 'automatic-feed-links' );
-		// Add Custom Menu
-		add_theme_support( 'menus' );
  }
 
+	/*
+	 * Clean up actions for
+		* - <head> tag
+		* - Loading theme `style.css` in posts
+		* - Updating sidebar
+		* - Add styles to the WYSIWYG editor
+		* - Add RSS links to <head> section
+		* - Add Custom Menu
+		* - Add post thumbnails to theme
+	 */
 	public function clean_up_actions() {
 		// Clean up the <head>
 		remove_action('wp_head', 'rsd_link');
 		remove_action('wp_head', 'wlwmanifest_link');
 		remove_action('wp_head', 'wp_generator');
-		// Add styles to the WYSIWYG editor. Function finds stylesheet from the root of the current theme's folder.
-		add_editor_style('style.css');
 
 		// Register sidebar
 		if (function_exists('register_sidebar')) {
@@ -35,9 +41,16 @@ class WPCoreUtils {
 				'after_title'   => '</h2>'
 			));
 		}		
+
+		add_editor_style('style.css');		
+  add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'menus' );
+		add_theme_support( 'post-thumbnails' );
 	}
 
- // Enqueue or dequeue styles and scripts
+	/*
+	 * Enqueue or dequeue styles and scripts
+	 */	
 	public function enqueue_dequeue_styles_scripts() {
 		$query_string = $this->query_string_from_timestamp();
 		// Dequeue WP block library CSS from WP Core
@@ -55,12 +68,11 @@ class WPCoreUtils {
 	}
 	
 	/* 
-	Update BODY tag classes including:
- - If on blog archive page (posts page) or single post add `blog` as a class
-	- If on any post (including custom post type) or page add `$post->post_name` value as a class	
-	- On single posts add category as body class (if present)
-	*/
-	
+	 * Update BODY tag classes including:
+  * - If on blog archive page (posts page) or single post add `blog` as a class
+	 * - If on any post (including custom post type) or page add `$post->post_name` value as a class	
+	 * - On single posts add category as body class (if present)
+	 */
 	public function customize_body_class($classes) {
 		global $post;
 		$new_classes = $classes;
@@ -85,8 +97,11 @@ class WPCoreUtils {
 			return $new_classes;
 	}
 
-	// Add page/post slug class to menu item classes AND
-	// replace current classes with "active"
+	/* 
+	 * Customize main menu out including:
+  * - Add page/post slug class to menu item classes
+	 * - replace current classes with "active"
+	 */
 	public function customize_nav_menu_output($output) {
 		$ps = get_option('permalink_structure');
 		if (!empty($ps)) {
@@ -117,15 +132,54 @@ class WPCoreUtils {
 		return $output;
 	}
 
-	// Limit the the_excerpt() to 20 words
-	public function custom_excerpt_length( $length ) {
-		return 20;
+	/* 
+	 * Excerpt updates
+	 */
+	public function improved_excerpt($text) {
+ 	global $post;
+
+  if ( '' == $text ) {
+			$text = get_the_content('');
+			$text = apply_filters('the_content', $text);
+			$text = str_replace('\]\]\>', ']]&gt;', $text);
+			$text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
+			$text = strip_tags($text, '<p> <em> <strong> <h2> <h3> <a>');
+			$text = $this->remove_all_html_tags_excluding_paragraphs($text);
+
+			$excerpt_length = 20;
+			$words = explode(' ', $text, $excerpt_length + 1);
+			// $words = $this->remove_all_html_tags_excluding_paragraphs($words);
+
+			if ( count($words) > $excerpt_length ) {
+				array_pop($words);
+				array_push($words, '&hellip;<br /><br /><a class="moretag" href="'. get_permalink($post->ID) . '">Read more</a>');
+				$text = implode(' ', $words);
+			}
+  }
+
+  return $text;
+ }
+
+	/*
+	* Post updates including:
+	* - Styling first post differently
+	*/
+	public function mark_first_post( $classes ) {
+		remove_filter( current_filter(), __FUNCTION__ );
+		$classes[] = 'first-post';
+		return $classes;
 	}
-	
- /**
-		* Private methods 
-		*/ 
- private function query_string_from_timestamp() {
+
+	/**
+	* Private helper methods
+	*/
+	// Replace headings tags (h1 thru h6) with paragraph tags WITHOUT any attributes
+	private function remove_all_html_tags_excluding_paragraphs( $content ) {
+ 	$content = preg_replace('/<h[1-6][^>]*>(.*?)<\/h[1-6]>/', '', $content);
+  return $content;
+ }
+
+	private function query_string_from_timestamp() {
 		$timestamp_json_file_path = get_stylesheet_directory() . '/build/json/timestamp.json';
 		$query_string = null;
 
@@ -135,6 +189,6 @@ class WPCoreUtils {
 			$timestamp = $obj->{'timestamp'};
 			$query_string = $timestamp;
 		}
-		return $query_string;
-	}	
-}
+			return $query_string;
+		}
+	}
