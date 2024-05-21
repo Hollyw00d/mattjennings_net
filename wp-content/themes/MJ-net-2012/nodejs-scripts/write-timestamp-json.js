5ca87977346c1ca9b6805ab892333570f2016159
Matt Jennings <matt@mattjennings.net>
2024-05-21 15:25:47 -0700

    removed css

--- wp-content/themes/MJ-net-2012/nodejs-scripts/write-timestamp-json.js
+++
@@ -1,7 +0,0 @@
-// Node.js code which creates/overwrites JSON file that includes a timestamp
-// which is used to append as query string onto child theme JS or CSS URLs to bust cache
-import fs from 'fs-extra';
-const file = `${process.cwd()}/build/json/timestamp.json`;
-const currentTime = Date.now();
-const obj = { timestamp: currentTime };
-fs.outputJsonSync(file, obj);
