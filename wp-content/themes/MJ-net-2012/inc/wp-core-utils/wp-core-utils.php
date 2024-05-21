0599c17d266fe4382cc5ae77469ef9402725c344
Matt Jennings <matt@mattjennings.net>
2024-05-21 15:10:37 -0700

    simplified css and js

--- wp-content/themes/MJ-net-2012/inc/wp-core-utils/wp-core-utils.php
+++ wp-content/themes/MJ-net-2012/inc/wp-core-utils/wp-core-utils.php
@@ -52,7 +52,6 @@ class WPCoreUtils {
 	 * Enqueue or dequeue styles and scripts
 	 */	
 	public function enqueue_dequeue_styles_scripts() {
-		$query_string = $this->query_string_from_timestamp();
 		// Dequeue WP block library CSS from WP Core
 		// and NOT on admin
 		if ( !is_admin() ) {
@@ -61,10 +60,10 @@ class WPCoreUtils {
 		}
 
 		// Enqueue CSS
-		wp_enqueue_style( 'theme-styles', get_stylesheet_directory_uri() . '/build/css/theme.min.css', array(), $query_string );
+		wp_enqueue_style( 'theme-styles', get_stylesheet_directory_uri() . '/build/css/theme.min.css', '', '', 'all');
 
 		// Enqueue JS
-		wp_enqueue_script('theme-scripts', get_stylesheet_directory_uri().'/build/js/theme.min.js', '', $query_string, true);
+		wp_enqueue_script('theme-scripts', get_stylesheet_directory_uri().'/build/js/theme.min.js', '', '', true);
 	}
 	
 	/* 
@@ -179,16 +178,4 @@ class WPCoreUtils {
   return $content;
  }
 
-	private function query_string_from_timestamp() {
-		$timestamp_json_file_path = get_stylesheet_directory() . '/build/json/timestamp.json';
-		$query_string = null;
-
-		if (file_exists($timestamp_json_file_path)) {
-			$json_data = file_get_contents($timestamp_json_file_path);
-			$obj = json_decode($json_data);
-			$timestamp = $obj->{'timestamp'};
-			$query_string = $timestamp;
-		}
-			return $query_string;
-		}
 	}
