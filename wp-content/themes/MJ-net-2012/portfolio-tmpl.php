a886148bb212d1d9015310ea6427cda6e8168702
Matt Jennings <matt@mattjennings.net>
2026-05-26 14:05:02 -0700

    removed tabindex

--- wp-content/themes/MJ-net-2012/portfolio-tmpl.php
+++ wp-content/themes/MJ-net-2012/portfolio-tmpl.php
@@ -138,7 +138,7 @@ Template Name: Portfolio Template
    <?php if( get_post_meta($post->ID, '_featured_project_radio', true) ): ?>
    <li class="featured-project<?php echo get_post_meta($post->ID, '_featured_project_order', true); ?>"
     data-featured-project-category="<?php echo $featured_portfolio_category; ?>">
-    <a href="<?php the_permalink(); ?>" tabindex="0">
+    <a href="<?php the_permalink(); ?>">
      <img src="<?php echo $image_thumbnail; ?>" alt="<?php the_title(); ?>" width="200" height="125" /><br />
      <?php echo get_post_meta($post->ID, '_portfolio_project_thumbnail_summary_line_1', true); ?><br /><?php echo get_post_meta($post->ID, '_portfolio_project_thumbnail_summary_line_2', true); ?><br /><?php echo get_post_meta($post->ID, '_portfolio_project_thumbnail_summary_line_3', true); ?></a>
    </li>
@@ -255,7 +255,7 @@ Template Name: Portfolio Template
         ?>
    <li data-project-category="<?php echo $portfolio_category; ?>"
     class="portfolio-project<?php echo get_post_meta($post->ID, '_non_featured_project_order', true); ?> hide-override">
-    <a href="<?php the_permalink(); ?>" tabindex="0">
+    <a href="<?php the_permalink(); ?>">
      <img src="<?php echo $image_thumbnail; ?>" alt="<?php the_title(); ?>" width="200" height="125"
       alt="<?php the_title(); ?>" width="200" height="125" /><br />
      <?php echo get_post_meta($post->ID, '_portfolio_project_thumbnail_summary_line_1', true); ?><br /><?php echo get_post_meta($post->ID, '_portfolio_project_thumbnail_summary_line_2', true); ?><br />
